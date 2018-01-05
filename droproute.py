a30adc40dd0075f018b7a9f42a06bf79892a88ea
Yoav Derman <dermanyoav@gmail.com>
2018-01-05 22:40:25 +0200

    Implemented terminal animotions

--- droproute.py
+++ droproute.py
@@ -1,5 +1,7 @@
+# -*- coding: utf-8 -*-
 from termcolor import colored
 from tabulate import tabulate
+import animation
 import uuid
 import digitalocean
 
@@ -13,35 +15,47 @@ __asciiart = """  ____                  ____             _
                                                     /_#/
  ver {}                                             /""".format(__version__)
 
+PULSE_ANIMATION = (
+    '[-------]', '[⌃------]', '[⌄^-----]', '[-⌄^----]', '[--⌄^---]',
+    '[---⌄^--]', '[----⌄^-]', '[-----⌄^]', '[------⌄]', '[-------]'
+)
+
+UUID = uuid.uuid4().hex
 
 class DropRoute(digitalocean.DigitalOcean):
 
     def __init__(self):
+        global UUID
+
         super(DropRoute, self).__init__()
-        self.uuid = uuid.uuid4().hex
-        self.tag = self.uuid
-        self.droplet_id = self.uuid.join(["-", "droplet"])
-        self.firewall_id = self.uuid.join(["-", "firewall"])
+        self.tag = UUID
+        self.droplet_id = "-".join([self.tag, "droplet"])
+        self.firewall_id = "-".join([self.tag, "firewall"])
 
 
     def __availability_color_mapping(self, row):
         if row[0]:
-            return [colored(row[1], 'blue'), colored(row[2], 'blue')]
+            return [colored(row[1], 'blue'), colored(row[2], 'cyan')]
         return [colored(row[1], 'red'), colored(row[2], 'red')]
 
     def display_available_regions(self):
-        response = self.api('get', 'regions')
-        regions_json = response['regions']
+        _loading = animation.Wait(text="Pending API Query", animation="bar")
+        _loading.start()
+        regions_json = self.api('get', 'regions')
+        regions_list = regions_json['regions']
+
         # Table headers (4 column list)
-        tab_headers = [['','Location', 'Datacenter']]
+        tab_headers = [['', 'Location', 'Datacenter']]
 
         # convert from Json to a List (column) of list (row)
-        tab_data = [[iter['available'], iter['name'], iter['slug']] for iter in regions_json]
+        tab_data = [[iter['available'], iter['name'].rsplit(' ', 1)[0], iter['slug']] for iter in regions_list]
 
         # termcoloring, converting True/False to Blue/Red colored rows
         colored_tab_data = map(self.__availability_color_mapping, tab_data)
 
+        _loading.stop()
         print(tabulate(tab_headers+colored_tab_data, showindex="always", headers="firstrow"))
+        return regions_list
 
 
     ## -- Assest allocation
@@ -55,10 +69,18 @@ class DropRoute(digitalocean.DigitalOcean):
         pass
     def destroy_firewall(self):
         pass
-    
-    def deploy_route(self):
+
+    def deploy_route(self, selected_datacenter):
+        _loading = animation.Wait(animation=PULSE_ANIMATION)
+        _loading.start()
+        print "[+] Selected datacenter: {}".format(colored(selected_datacenter['slug'], "cyan"))
+
         self.deploy_firewall() #First in
         self.deploy_droplet()
+        import time; time.sleep(10) #todo remove this
+        print "[+] Done!"
+        _loading.stop()
+        return True
          
     def destroy_route(self):
         self.destroy_droplet()
@@ -82,10 +104,24 @@ def load_client_locally(client_config):
     #Todo: load client config to local ovpn bin
     pass
 
+def prompt_select(display_message, option_list):
+    # list --> Chosen selection index
+    selection = raw_input(" ".join(["-->",
+                                    display_message,
+                                    "(1-{})".format(len(option_list)-1),
+                                    ":"]))
+    if not selection.isdigit() or not len(option_list)> int(selection) >= 0:
+        print "#ERR: Supplied an invalid option!"
+        return prompt_select(display_message, option_list)
+    return int(selection)
+
 def main():
     print colored(__asciiart, 'yellow')
     Digimon = DropRoute()
-    Digimon.display_available_regions()
+    datacenter_list = Digimon.display_available_regions()
+    selected_region_index = prompt_select("Select region", datacenter_list)
+
+    Digimon.deploy_route(datacenter_list[selected_region_index])
 
 
 if __name__ == '__main__':
